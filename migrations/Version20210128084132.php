b7623d86c0d7aac454d6cb7fb23ce10296687f59
maxime.herblot <maximeherblot123@gmail.com>
2021-01-28 09:57:11 +0100

    Changement des migrations

--- migrations/Version20210128084132.php
+++ migrations/Version20210128084132.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210127211526 extends AbstractMigration
+final class Version20210128084132 extends AbstractMigration
 {
     public function getDescription() : string
     {
