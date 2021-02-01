c72a0ff7527db78e638926d053fa3032bc47b818
maxime.herblot <maximeherblot123@gmail.com>
2021-02-01 21:41:34 +0100

    Refactorisation test

--- Testing_file.php
+++ Testing_file.php
@@ -1,68 +1,17 @@
-<?php
-//Ouverture du fichier
-$file2 = (file(".env"));
-//Donnée que l'on cherche
-$debut="DATABASE_URL=";
+<?php 
 
-$length = strlen("DATABASE_URL=");
 
-//Récupération de la ligne 
-$i=0;
-foreach ($file2 as $lineFile){
-    
-    if (substr($lineFile,0,$length)==$debut) {
-        
-        $indiceLine = $i;
-        
+class test {
+    public function b(){
+        $value = 1;
     }
-    $i++;
-}
-//Récupération des données dans le .env de la database
-
-$infoDBNonFiltre = substr($file2[$indiceLine],$length);
-$infoDBNonFiltre = substr($infoDBNonFiltre,0,strlen($infoDBNonFiltre)-1);
-var_dump($infoDBNonFiltre);
-//Récupération du type de base de donnée
-$nameDB = substr($infoDBNonFiltre,0,strpos($infoDBNonFiltre,":"));
-
-//Execution de différents filtreurs pour pouvoir avoir les  autres informations nécessaires
-
-if (strtoupper($nameDB)=="SQLITE") {
-    $donnee = filtreSQLite($infoDBNonFiltre);
-} else if (strtoupper($nameDB)=="POSTGRESQL") {
-    $donnee = filtrePostgreSQL($infoDBNonFiltre);
-} else if (strtoupper($nameDB)=="MYSQL") {
-    $donnee = filtreMySql($infoDBNonFiltre);
-}
-
-
-function filtreSQLite($infoDBNonFiltre){
-
-}
-
-function filtrePostgreSQL($infoDBNonFiltre){
-
-}
-// echo $infoDBNonFiltre;
-function filtreMySql($infoDBNonFiltre){
+    public function a (){
+        $this->b();
+        echo($value);
+    }
     
-    //Récupération de l'identifiant
-    //Récupération du mot de passe
-    $log = substr($infoDBNonFiltre,8,strpos($infoDBNonFiltre,"@")-8);
-    $identifiant = substr($log,0,strpos($log,":"));
-    $password = substr($log,strpos($log,":")+1);
-    
-    //Récupération de l'host
-    $hostPortName= substr($infoDBNonFiltre,strpos($infoDBNonFiltre,"@")+1);
-    $host = substr($hostPortName,0,strpos($hostPortName,":"));
-    //Récupération de la dbname
-    $dbname = substr($hostPortName,strpos($hostPortName,"/")+1);
-    $dbname = substr($dbname,0,strlen($dbname)-1);
-    $port = substr($hostPortName,strpos($hostPortName,":"),5);
-    return ["namedb"=>"mysql","id"=>$identifiant,"password"=>$password,"host"=>$host,"dbname"=>$dbname,"port"=>$port];
 }
-$dsn=$donnee["namedb"].":"."host=".$donnee["host"].$donnee["port"].";dbname=".$donnee["dbname"];
-
-$conn = new PDO($dsn,$donnee["id"],$donnee["password"]);
 
 
+$class = new test();
+$class->a();
--- tests/Controller/CreationCharactersControllerTest.php
+++ tests/Controller/CreationCharactersControllerTest.php
@@ -8,13 +8,7 @@ use function PHPUnit\Framework\assertEquals;
 
 class CreationCharactersControllerTest extends TestCase{
 
-    /**
-     * @test
-     */
-    public function verification_characters_bien_creer ( ) {
-
-        //Test de la function index du controller
-        //Initialisation des valeurs que l'on veut
+    public function creation_value_random(){
         $faker = Faker\Factory::create();
         $FirstNameLastName=$faker->name();
         $lastname = $_POST["lastname"] = explode(" ",$FirstNameLastName)[1];
@@ -22,7 +16,10 @@ class CreationCharactersControllerTest extends TestCase{
         $description = $_POST["Description"] = $faker->text(50);
         $age = $_POST["age"] = rand(15,300);
 
+        return ["age"=>$age,"description"=>$description,"firstname"=>$firstname,"lastname"=>$lastname];
+    }
 
+    public function recup_user(){
         $classConn = new ConnectionBdClass();
         $conn = $classConn->getConnection();
 
@@ -30,7 +27,7 @@ class CreationCharactersControllerTest extends TestCase{
 
         // $conn = new PDO("mysql:host=localhost:3306;dbname=dndinit","root","");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+        //Récupération de l'id d'un des user
         $stmt= $conn->prepare("SELECT id FROM user");
         $stmt ->execute();
         $stmt->setFetchMode(PDO::FETCH_CLASS,'User');
@@ -42,28 +39,64 @@ class CreationCharactersControllerTest extends TestCase{
         } else {
             $userId = $_POST["userId"]=($fetch[rand(0,count($fetch)-1)]["id"]);
         }
-        $stmt= $conn->prepare("SELECT id FROM race");
-        $stmt ->execute();
-        $stmt->setFetchMode(PDO::FETCH_CLASS,'Race');
+        return $userId;
+    }
 
-        $fetch = $stmt->fetchAll();
-        
-        if (!$fetch) {
-            $raceId =NULL;
-        } else {
-            $raceId = $_POST["raceId"]=($fetch[rand(0,count($fetch)-1)]["id"]);
-        }
-        // dd($raceid,$userId);
-        //Action qui va se passé 
-        //Un personnage a un prenom un nom un age une description
 
-        //Création de différentes entité avec faker
+public function recup_race(){
+    $classConn = new ConnectionBdClass();
+    $conn = $classConn->getConnection();
+
+
+
+    // $conn = new PDO("mysql:host=localhost:3306;dbname=dndinit","root","");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //Récupération de l'id d'une des races
+    $stmt= $conn->prepare("SELECT id FROM race");
+    $stmt ->execute();
+    $stmt->setFetchMode(PDO::FETCH_CLASS,'Race');
+
+    $fetch = $stmt->fetchAll();
         
+    if (!$fetch) {
+        $raceId =NULL;
+    } else {
+        $raceId = $_POST["raceId"]=($fetch[rand(0,count($fetch)-1)]["id"]);
+    }
+    return $raceId;
+    
+}
+
+    /**
+     * @test
+     */
+    public function verification_characters_bien_creer ( ) {
+
+        //Test de la function index du controller
+        //Initialisation des valeurs que l'on veut
+        // $faker = Faker\Factory::create();
+        // $FirstNameLastName=$faker->name();
+        // $lastname = $_POST["lastname"] = explode(" ",$FirstNameLastName)[1];
+        // $firstname = $_POST["firstname"] = explode(" ",$FirstNameLastName)[0];
+        // $description = $_POST["Description"] = $faker->text(50);
+        // $age = $_POST["age"] = rand(15,300);
+
+        $listValue = $this->creation_value_random();
+
+        $age = $listValue["age"];
+        $description = $listValue["description"];
+        $firstname = $listValue["firstname"];
+        $lastname = $listValue["lastname"];
+
+        $userId = $this->recup_user();
+        $raceId = $this->recup_race();
+
+        $classConn = new ConnectionBdClass();
+        $conn = $classConn->getConnection();
+
         $controller = new CreationCharactersController();
         $infoCreation = $controller->creationPersonnage();
 
-        
-        
         //Alors on vérifie que les données qui sont enregistré sont les bonnes
        
         $stmt =$conn->prepare("SELECT * FROM characters WHERE lastname LIKE '$lastname' AND firstname LIKE '$firstname' AND age LIKE '$age' AND user_id LIKE '$userId' AND race_id LIKE '$raceId'");
@@ -71,6 +104,7 @@ class CreationCharactersControllerTest extends TestCase{
 
         $stmt->setFetchMode(PDO::FETCH_CLASS,"Characters",[]);
         $fetch = $stmt->fetchAll()[0];
+        var_dump($fetch);
         assertEquals($lastname,$fetch["lastname"]);
         assertEquals($firstname,$fetch["firstname"]);
         assertEquals($age,$fetch["age"]);
@@ -79,11 +113,31 @@ class CreationCharactersControllerTest extends TestCase{
         assertEquals($userId,$fetch["user_id"]);
         assertEquals($raceId,$fetch["race_id"]);
 
-        // assertEquals($lastname,$infoCreation->{"lastname"});
-        // assertEquals($firstname,$infoCreation->{"firstname"});
-        // assertEquals($age,$infoCreation->{"age"});
-        // assertEquals($description,$infoCreation->{"description"});
-        // //Vérification que l'utilisateur a bien le personnage
-        // assertEquals($userId,$infoCreation->{"id"});
+        // // $conn = new PDO("mysql:host=localhost:3306;dbname=dndinit","root","");
+        // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // //Récupération de l'id d'un des user
+        // $stmt= $conn->prepare("SELECT id FROM user");
+        // $stmt ->execute();
+        // $stmt->setFetchMode(PDO::FETCH_CLASS,'User');
+
+        // $fetch = $stmt->fetchAll();
+        
+        // if (!$fetch) {
+        //     $userId =NULL;
+        // } else {
+        //     $userId = $_POST["userId"]=($fetch[rand(0,count($fetch)-1)]["id"]);
+        // }
+
+        //Récupération de l'id d'une des race dans la base de données
+        
+        // dd($raceid,$userId);
+        
+        
+        
+
+        
+        
+        
+        
     }
 }
